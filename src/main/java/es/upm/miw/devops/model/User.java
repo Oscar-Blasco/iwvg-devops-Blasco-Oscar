6a39f9eb778a3191e91780ce8c3476e1b1965e28
O.Blasco <oscar.blasco@alumnos.upm.es>
2026-09-12 12:24:38 +0200

    [#10] Update User.java and UserResourceFT.java

--- src/main/java/es/upm/miw/devops/model/User.java
+++ src/main/java/es/upm/miw/devops/model/User.java
@@ -1,7 +1,13 @@
 package es.upm.miw.devops.model;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
     private String id;
     private String name;
     private String familyName;
--- src/test/java/es/upm/miw/devops/functionaltests/UserResourceFT.java
+++ src/test/java/es/upm/miw/devops/functionaltests/UserResourceFT.java
@@ -1,15 +1,25 @@
 package es.upm.miw.devops.functionaltests;
 
-import org.hamcrest.Matchers;
+import es.upm.miw.devops.model.User;
+import es.upm.miw.devops.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,12 +29,21 @@ class UserResourceFT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+        userRepository.save(new User("1", "Oscar", "Blasco"));
+    }
+
     @Test
     void testReadExistingUser() throws Exception {
-        mockMvc.perform(get("/users/1"))
+        mockMvc.perform(get("/user/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("user"))
-                .andExpect(model().attributeExists("user"))
-                .andExpect(content().string(Matchers.containsString("Oscar")));
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Oscar"))
+                .andExpect(jsonPath("$.familyName").value("Blasco"));
     }
 }
